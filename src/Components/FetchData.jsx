5c94180f7407c2610fe1c416218412d7cbb3ce46
kellishbond <kellishbond@gmail.com>
2024-07-23 16:44:18 +0100

    reverted to the pre

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -6,7 +6,7 @@ import './FetchData.css'
 
 const FetchData = () => {
   
-const [data]=UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
+const [data]=UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
 console.log(data);
   return (
     <>
@@ -17,9 +17,8 @@ console.log(data);
       <>
       <li className='list_data'>
             <h3>{e.name}</h3>
-            <p><strong>Importance: </strong>{e.importance}</p>
-            <p><strong>Benefits: </strong>{e.benefits6}</p>
-            <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+            <p><strong>Benefits: </strong>{e.benefits}</p>
+            <p><strong>Time to eat: </strong>{e.time_duration}</p>
             
             </li>
       </>
